5afef2f087690171113decc3bdb4aa8a9ee4f7fd
MADHAV KALRA <68096582+madhavkalra7@users.noreply.github.com>
2026-03-27 23:15:18 +0530

    Fix Vercel static manifest crash with WhiteNoise finders

--- youtify/settings.py
+++ youtify/settings.py
@@ -162,7 +162,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+WHITENOISE_USE_FINDERS = True
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
